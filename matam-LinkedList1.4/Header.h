23451eca13ab9f4301ebfd4e0263aea58d61cf2f
Tom Goldberg <tomg3210@gmail.com>
2016-09-09 11:56:22 +0300

    Update Header.h

--- matam-LinkedList1.4/Header.h
+++ matam-LinkedList1.4/Header.h
@@ -9,17 +9,7 @@
 enum sign { Hearts, Diamonds, Clubs, Spades };
 enum color { Red, Black };
 
-
-const char* getCardSign(enum sign signs);				// getter for the sign of the card
+const char* getCardSign(enum sign signs);				  // getter for the sign of the card
 const char* getCardColor(enum color colors);			// getter for the color of the card
-
-
-
-
-
-
-
-
-
 
 #endif
