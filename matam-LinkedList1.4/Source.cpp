ed2c486a1a0c12a11c43dc054d7551c295c65e25
Tom Goldberg <tomg3210@gmail.com>
2016-09-09 11:57:19 +0300

    Update Source.cpp

--- matam-LinkedList1.4/Source.cpp
+++ matam-LinkedList1.4/Source.cpp
@@ -1,21 +1,17 @@
 #include "Header.h"
 
 const char* getCardSign(enum sign signs)				// getter for the sign od the card
-{
-	switch (signs)										// switch case of signs
-	{
-	case Hearts: return "Hearts";
-	case Diamonds: return "Diamonds";
-	case Clubs: return "Clubs";
-	case Spades: return "Spades";
+	switch (signs)							// switch case of signs
+		case Hearts: return "Hearts";
+		case Diamonds: return "Diamonds";
+		case Clubs: return "Clubs";
+		case Spades: return "Spades";
 	}
 }
 
 const char* getCardColor(enum color colors)				// getter for the color od the card
-{
-	switch (colors)										// switch case of colors
-	{
-	case Red: return "Red";
-	case Black: return "Black";
+	switch (colors)							// switch case of colors
+		case Red: return "Red";
+		case Black: return "Black";
 	}
 }
